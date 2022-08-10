3a20f4528367e43df91055f444fb64d25a4b62d2
bayareahomelander <110600178+bayareahomelander@users.noreply.github.com>
2022-08-09 19:25:56 -0700

    Update and rename 242. Valid Anagram to 242. Valid Anagram.py

--- Python/242. Valid Anagram.py
+++ Python/242. Valid Anagram.py
@@ -2,5 +2,7 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return (sorted(s) == sorted(t)) # 'car' & 'rat' would be -> ['a', 'c', 'r'] & ['a', 'r', 't']
 
+'''
 Time: O(nlogn)
 Space: O(n)
+'''
