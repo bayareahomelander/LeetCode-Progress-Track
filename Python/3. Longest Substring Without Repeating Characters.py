21d5b9556a0f6f95a6852a88fc6bbf61c4d48a0c
bayareahomelander <110600178+bayareahomelander@users.noreply.github.com>
2022-08-10 18:09:23 -0700

    Create 3. Longest Substring Without Repeating Characters.py

---
+++ Python/3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        output = 0
+        record = [] # Empty array to keep track of unique characters.
+        
+        for i in s:
+            while i in record:
+                record.pop(0) # If a letter is already in the array, remove the leftmost character.
+             
+            record.append(i) # Else simply add the character.
+            output = max(output, len(record)) # Constantly update the maximum value
+        
+        return output
+
+'''
+Time: O(n)
+Space: O(n)
+'''
