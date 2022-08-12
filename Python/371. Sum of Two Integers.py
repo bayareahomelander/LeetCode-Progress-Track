9a3abce2a8a956f0ea3accd15fc49f94b696ac5e
bayareahomelander <110600178+bayareahomelander@users.noreply.github.com>
2022-08-11 18:12:41 -0700

    Update 371. Sum of Two Integers.py

--- Python/371. Sum of Two Integers.py
+++ Python/371. Sum of Two Integers.py
@@ -1,8 +1,8 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        return sum([a,b]) #...Medium question?
+        return sum([a,b])
 
 '''
-Time: O(1) (?) since it's always two integers and the length of list is not arbitrary.
+Time: O(1) since it's always two integers and the length of list is non-arbitrary.
 Space: O(n)
 '''
