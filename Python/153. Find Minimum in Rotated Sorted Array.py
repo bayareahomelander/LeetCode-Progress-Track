18fb1f3dcff3f84889c6f1d567ab51394afa7dcb
bayareahomelander <110600178+bayareahomelander@users.noreply.github.com>
2022-08-23 15:46:03 -0700

    Update 153. Find Minimum in Rotated Sorted Array.py

--- Python/153. Find Minimum in Rotated Sorted Array.py
+++ Python/153. Find Minimum in Rotated Sorted Array.py
@@ -7,9 +7,10 @@ class Solution:
             mid = (l+r) // 2
             
             if nums[mid] > nums[r]: # Compare mid with the rightmost value, if greater then array must've been rotated.
+                                    # Therefore search the right portion of the array.
                 l = mid+1
                 
-            else: # Else array may be sorted in ascending order, move right pointer to midpoint.
+            else: # Else array may be sorted in ascending order, move right pointer to midpoint - search the left portion.
                 r = mid
                 
         return nums[l]
