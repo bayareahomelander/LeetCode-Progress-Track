2012f8e69f185a2a7bb1e2480a4fb889ad5c9e45
bayareahomelander <110600178+bayareahomelander@users.noreply.github.com>
2022-09-06 00:40:00 -0700

    Create 50. Pow(x, n).py

---
+++ Python/50. Pow(x, n).py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def helper(x, n):
+            if x == 0: return 0
+            if n == 0: return 1
+            
+            output = helper(x * x, n // 2)
+            return x * output if n % 2 else output
+        
+        output = helper(x, abs(n))
+        return output if n >= 0 else 1/output
+      
+'''
+Time: O(logn)
+Space: O(2^n)
+'''
