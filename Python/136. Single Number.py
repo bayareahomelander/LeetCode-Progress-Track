f1f5c12b19bf4c06bee972e106e3e6e7cc042f9d
bayareahomelander <110600178+bayareahomelander@users.noreply.github.com>
2022-09-13 13:35:20 -0700

    Create 136. Single Number.py

---
+++ Python/136. Single Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        output = 0
+        
+        for i in nums:
+            output ^= i
+            
+        return output
+      
+'''
+Time: O(n)
+Space: O(1)
+'''
