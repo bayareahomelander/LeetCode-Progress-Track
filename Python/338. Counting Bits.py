44ddca4381e1a934af705432b3502350b954e297
bayareahomelander <110600178+bayareahomelander@users.noreply.github.com>
2022-09-20 19:58:02 -0700

    Create 338. Counting Bits.py

---
+++ Python/338. Counting Bits.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        
+        output = [0]
+        
+        for i in range(1, n+1):
+            output.append(self.helper(i))
+            
+        return output
+            
+    
+    def helper(self, n):
+        m = str(int('{0:032b}'.format(n)))
+        ones = m.count('1')
+        
+        return ones
+
+'''
+Time: O(nlogn)
+Space: O(n)
+'''
