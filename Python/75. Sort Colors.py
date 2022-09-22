27a4275d58ed0823b34816ee9a2c4502129676d8
bayareahomelander <110600178+bayareahomelander@users.noreply.github.com>
2022-09-21 22:17:18 -0700

    Create 75. Sort Colors.py

---
+++ Python/75. Sort Colors.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        for l in range(len(nums)):
+            for r in range(l + 1, len(nums)):
+                if nums[l] > nums[r]:
+                    nums[l], nums[r] = nums[r], nums[l]
+                    
+'''
+Time: O(n^2)
+Space: O(1)
+'''
