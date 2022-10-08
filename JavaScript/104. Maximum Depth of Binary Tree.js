d4c3c3cdfbf3c7379c054b2dfb8142b6ad93e65c
bayareahomelander <110600178+bayareahomelander@users.noreply.github.com>
2022-10-07 21:11:40 -0700

    Create 104. Maximum Depth of Binary Tree.js

---
+++ JavaScript/104. Maximum Depth of Binary Tree.js
@@ -0,0 +1,15 @@
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+  if (root == undefined || root == null) {
+    return 0;
+  }
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+
+/*
+Time: O(n)
+Space: O(n)
+*/
